30b34ec8f67b12a6ee7ababb7f611caed11eacfd
Arley Cuadrado Sierra <arley.cuadradosierra@gmail.com>
2026-05-05 17:58:34 -0500

    dynamic date added

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -1,38 +1,21 @@
 import { getCachedGlobal } from '@/utilities/getGlobals'
 import Link from 'next/link'
-import React from 'react'
-
-import { CMSLink } from '@/components/Link'
-import { Logo } from '@/components/Logo/Logo'
 
 export async function Footer() {
   const footerData = await getCachedGlobal('footer', 1)()
+  const currentYear = new Date().getFullYear()
 
   const navItems = footerData?.navItems || []
 
   return (
     <footer className="mt-auto bg-white dark:bg-card text-dark">
       {/* border-t border-border */}
-      <div className="container py-8 gap-8 flex flex-col md:flex-col items-center">
-        <div>
-          <div className="flex flex-col-reverse">
-            {/*
-            <nav className="flex flex-col md:flex-row gap-4">
-              {navItems.map(({ link }, i) => {
-                return <CMSLink className="text-white" key={i} {...link} />
-              })}
-            </nav>
-            */}
-          </div>
-        </div>
+      <div className="container gap-8 flex flex-col md:flex-col items-center pt-8">
         <div>
           <Link href="https://www.instagram.com/arlo_cuadrado/" target="_blank">
             <p>
-              © 2026 | <span className="text-red title">@arlo_cuadrado</span>
-            </p>
-            {/*<Logo />*/}
+            © {currentYear} | Made with love and lots of Coffee</p>
           </Link>
-          <p>Made with love and lots of Coffee</p>
         </div>
       </div>
     </footer>
--- src/app/(frontend)/home-components/AsideHome.tsx
+++ src/app/(frontend)/home-components/AsideHome.tsx
@@ -1,55 +1,69 @@
-import Link from "next/link"
-import Image from "next/image"
-import { Footer } from "@/Footer/Component"
+import Link from 'next/link'
+import Image from 'next/image'
+import { Footer } from '@/Footer/Component'
 
 export default function AsideHome() {
-    return (
-        <>
-            <aside className="gap-6 mb-7 pt-4 pb-4">
-            <Link href={'/about-us'}>
-                        <div className="w-70 px-6">
-                            <div>
-                                <h2 className="font-semibold tracking-tight text-pretty text-slate-700 dark:text-white">Binevenid@!</h2>
-                                <p className="mt-2 text-slate-600 dark:text-gray-400">Esta es una versión temprana del sitio, un espacio donde encontrarás artistas independientes.</p>
-                            </div>
-                            <div className="grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                                <article className="flex max-w-xl flex-col items-start justify-between">
-                                    <div className="flex items-center gap-x-4 text-xs text-slate-600 dark:text-gray-400">
-                                        <time dateTime="2020-03-16">Apr 17, 2026</time>
-                                        {/*<Link href="/about-us" className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">CTO</Link>*/}
-                                    </div>
-                                    <div className="group relative grow">
-                                        <h3 className="mt-3 text-lg/6 font-semibold text-slate-700 dark:text-white">
-                                            {/*<a href={'/about-us'}>*/}
-                                            <span className="absolute inset-0"></span>
-                                            Por qué Oddsound?
-                                            {/*</a>*/}
-                                        </h3>
-                                        <p className="mt-5 line-clamp-3 text-sm/6 text-slate-600 dark:text-gray-400">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
-                                    </div>
-                                    <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
-                                        <Image
-                                            src="/home-images/arlo_cuadrado.png"
-                                            alt="Arlo Cuadrado"
-                                            width={40}
-                                            height={40}
-                                            className="size-10 rounded-full"
-                                        />
-                                        <div className="text-sm/6">
-                                            <p className="font-semibold text-slate-700 dark:text-gray-400">
-                                                {/*<a href={'/about-us'}>*/}
-                                                <span className="absolute inset-0"></span>
-                                                Arlo Cuadrado
-                                                {/*</a>*/}
-                                            </p>
-                                            <p className="text-slate-600 dark:text-gray-400">Founder / &quot;Content Creator&quot;</p>
-                                        </div>
-                                    </div>
-                                </article>
-                            </div>
-                        </div>
-            </Link>
-            <Footer />
-            </aside>
-        </>)
+  return (
+    <>
+      <aside className="gap-6 mb-7 pt-4 pb-4">
+        <div className="w-70 px-6">
+          <div>
+            <h2 className="font-semibold tracking-tight text-pretty text-slate-700 dark:text-white">
+              Binevenid@!
+            </h2>
+            <p className="mt-2 text-slate-600 dark:text-gray-400">
+              Esta es una versión temprana del sitio, un espacio donde encontrarás artistas
+              independientes.
+            </p>
+          </div>
+          <div className="grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <article className="flex max-w-xl flex-col items-start justify-between">
+              <div className="flex items-center gap-x-4 text-xs text-slate-600 dark:text-gray-400">
+                <time dateTime="2020-03-16">Apr 17, 2026</time>
+                {/*<Link href="/about-us" className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">CTO</Link>*/}
+              </div>
+              <Link href={'/about-us'}>
+                <div className="group relative grow">
+                  <h3 className="mt-3 text-lg/6 font-semibold text-slate-700 dark:text-white">
+                    {/*<a href={'/about-us'}>*/}
+                    <span className="absolute inset-0"></span>
+                    Por qué Oddsound?
+                    {/*</a>*/}
+                  </h3>
+                  <p className="mt-5 line-clamp-3 text-sm/6 text-slate-600 dark:text-gray-400">
+                    Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo.
+                    Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat
+                    consectetur nulla deserunt vel. Iusto corrupti dicta.
+                  </p>
+                </div>
+              </Link>
+              <Link href="https://www.instagram.com/arlo_cuadrado/" target="_blank">
+                <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
+                  <Image
+                    src="/home-images/arlo_cuadrado.png"
+                    alt="Arlo Cuadrado"
+                    width={40}
+                    height={40}
+                    className="size-10 rounded-full"
+                  />
+                  <div className="text-sm/6">
+                    <div>
+                      <p>
+                        <span className="text-red title">@arlo_cuadrado</span>
+                      </p>
+
+                      <p className="text-slate-600 dark:text-gray-400 text-xs">
+                        Founder / &quot;Content Creator&quot;
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </article>
+          </div>
+        </div>
+        <Footer />
+      </aside>
+    </>
+  )
 }
