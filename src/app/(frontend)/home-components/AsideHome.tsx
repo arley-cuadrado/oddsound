90658f9ee870c15a45ecd38f05aa05636fce66c8
Arley Cuadrado Sierra <arley.cuadradosierra@gmail.com>
2026-05-05 10:14:11 -0500

    aside component added

---
+++ src/app/(frontend)/home-components/AsideHome.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link"
+import Image from "next/image"
+
+export default function AsideHome() {
+    return (
+        <>
+            <Link href={'/about-us'}>
+                <aside className="gap-6 mb-7 border-l border-gray-100">
+                    <div className="pb-24 sm:pb-32">
+                        <div className="w-100 px-6">
+                            <div>
+                                <h2 className="font-semibold tracking-tight text-pretty text-slate-700 dark:text-white">Binevenid@!</h2>
+                                <p className="mt-2 text-slate-600 dark:text-gray-400">Esta es una versión temprana del sitio, un espacio donde encontrarás artistas independientes.</p>
+                            </div>
+                            <div className="grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                                <article className="flex max-w-xl flex-col items-start justify-between">
+                                    <div className="flex items-center gap-x-4 text-xs text-slate-600 dark:text-gray-400">
+                                        <time dateTime="2020-03-16">Apr 17, 2026</time>
+                                        {/*<Link href="/about-us" className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">CTO</Link>*/}
+                                    </div>
+                                    <div className="group relative grow">
+                                        <h3 className="mt-3 text-lg/6 font-semibold text-slate-700 dark:text-white">
+                                            {/*<a href={'/about-us'}>*/}
+                                            <span className="absolute inset-0"></span>
+                                            Por qué Oddsound?
+                                            {/*</a>*/}
+                                        </h3>
+                                        <p className="mt-5 line-clamp-3 text-sm/6 text-slate-600 dark:text-gray-400">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+                                    </div>
+                                    <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
+                                        <Image
+                                            src="/home-images/arlo_cuadrado.png"
+                                            alt="Arlo Cuadrado"
+                                            width={40}
+                                            height={40}
+                                            className="size-10 rounded-full"
+                                        />
+                                        <div className="text-sm/6">
+                                            <p className="font-semibold text-slate-700 dark:text-gray-400">
+                                                {/*<a href={'/about-us'}>*/}
+                                                <span className="absolute inset-0"></span>
+                                                Arlo Cuadrado
+                                                {/*</a>*/}
+                                            </p>
+                                            <p className="text-slate-600 dark:text-gray-400">Founder / &quot;Content Creator&quot;</p>
+                                        </div>
+                                    </div>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                </aside>
+            </Link>
+
+        </>)
+}
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,10 +1,11 @@
+import AsideHome from './home-components/AsideHome'
 import HeaderHome from './home-components/HeaderHome'
 
 export default function HomePage() {
   return (
     <>
       <HeaderHome />
-      <main>
+      <main className='flex flex-row items-center justify-center h-screen'>
         <section className="px-6 py-16 md:px-10 lg:px-16">
           <div className="mx-auto max-w-5xl">
             <h2 className="text-2xl font-semibold md:text-4xl">Welcome to ODDSOUND</h2>
@@ -13,6 +14,7 @@ export default function HomePage() {
             </p>
           </div>
         </section>
+        <AsideHome />
       </main>
     </>
   )
