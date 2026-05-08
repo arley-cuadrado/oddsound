876989a8dc7aed352c5c04b67051a79a8c300f1d
Arley Cuadrado Sierra <arley.cuadradosierra@gmail.com>
2026-05-07 22:34:32 -0500

    pages padding top removed

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -67,7 +67,7 @@ export default async function Page({ params: paramsPromise }: Args) {
   const { hero, layout } = page
 
   return (
-    <article className="mx-auto max-w-4xl pt-16 pb-24">
+    <article className="mx-auto max-w-4xl pb-24">
       <PageClient />
       {/* Allows redirects for valid pages too */}
       <PayloadRedirects disableNotFound url={url} />
