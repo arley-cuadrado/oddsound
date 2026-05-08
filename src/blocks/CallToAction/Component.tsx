0c39b8b34c0c84175e553488539ac382f4bb0ee8
Arley Cuadrado Sierra <arley.cuadradosierra@gmail.com>
2026-05-08 05:43:40 -0500

    gradient in CTA background

--- src/blocks/CallToAction/Component.tsx
+++ src/blocks/CallToAction/Component.tsx
@@ -8,13 +8,19 @@ import { CMSLink } from '@/components/Link'
 export const CallToActionBlock: React.FC<CTABlockProps> = ({ links, richText }) => {
   return (
     <div className="container">
-      <div className="bg-card rounded border-border border p-4 flex flex-col gap-8 md:flex-row md:justify-between md:items-center">
+      <div className="flex flex-col gap-8 bg-[length:400%_400%] bg-[linear-gradient(-45deg,#ee7752,#e73c7e,#23a6d5,#23d5ab)] p-4 [animation:gradient_15s_ease_infinite] md:flex-row md:items-center md:justify-between">
         <div className="max-w-[48rem] flex items-center">
-          {richText && <RichText className="mb-0" data={richText} enableGutter={false} />}
+          {richText && (
+            <RichText
+              className="mb-0 [&_*]:text-white [&_a]:text-white [&_li::marker]:text-white"
+              data={richText}
+              enableGutter={false}
+            />
+          )}
         </div>
         <div className="flex flex-col gap-8">
           {(links || []).map(({ link }, i) => {
-            return <CMSLink key={i} size="lg" {...link} />
+            return <CMSLink key={i} size="lg" className="border-0 shadow-none" {...link} />
           })}
         </div>
       </div>
