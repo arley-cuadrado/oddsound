d66e1e9210e1cfd0cb36b496b1f9869c9fdcaa58
Arley Cuadrado Sierra <arley.cuadradosierra@gmail.com>
2026-05-08 05:23:29 -0500

    the borders of the multimedia block were removed

--- src/blocks/MediaBlock/Component.tsx
+++ src/blocks/MediaBlock/Component.tsx
@@ -44,7 +44,7 @@ export const MediaBlock: React.FC<Props> = (props) => {
     >
       {(media || staticImage) && (
         <Media
-          imgClassName={cn('border border-border rounded-[0.8rem]', imgClassName)}
+          imgClassName={cn('border-0 rounded-none', imgClassName)}
           resource={media}
           src={staticImage}
         />
