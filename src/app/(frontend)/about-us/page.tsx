bbcba260bbcb5bf815ee4cbda802bde01c8c23fe
Arley Cuadrado Sierra <arley.cuadradosierra@gmail.com>
2026-05-16 11:22:26 -0500

    about us italic - em tag added

--- src/app/(frontend)/about-us/page.tsx
+++ src/app/(frontend)/about-us/page.tsx
@@ -9,12 +9,15 @@ const sections = [
     body: [
       'Tener parte de tu estrategía de marketing en tiktok, instagram, pero a la vez usar youtube para ver videos y las diversas plataformas de streaming musicales a las que hay que tener acceso para escucharte, hacen que el proceso de descubrimiento sea un tanto extenso, por lo que, por qué no tener un espacio que pueda funcionar como sitio web o landing page donde puedas sintetizar todo eso? Ojo! SINTETIZAR…',
       'Puede que a primera vista el sitio no resulte en algo innovador y es que tampoco lo pretende, solo quiere decirle a los independientes:',
-      '“Hey, estoy aquí! Llégate y sube tu info y lanzamientos si quieres, si puedes y si te da la gana… no pierdes na’, a la final todos moriremos.”',
+      <em key="about-us-quote">
+        “Hey, estoy aquí! Llégate y sube tu info y lanzamientos si quieres, si puedes y si te da la
+        gana… no pierdes na’, a la final todos moriremos.”
+      </em>,
     ],
   },
   {
     title: 'Es gratis...',
-    body: ['Es la primera versión del sitio, por lo que puede que haya cosas por pulir.'],
+    body: ['Es la primera versión de la plataforma, por lo que puede que haya cosas por pulir.'],
   },
   {
     title: '',
