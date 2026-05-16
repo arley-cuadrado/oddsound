eb878fa47cfa540fb4dbc458696a04a517341120
Arley Cuadrado Sierra <arley.cuadradosierra@gmail.com>
2026-05-16 18:44:15 -0500

    title margen added

--- src/app/(frontend)/home-components/ReleasesHome.tsx
+++ src/app/(frontend)/home-components/ReleasesHome.tsx
@@ -150,7 +150,7 @@ export default function ReleasesHome({ releases }: { releases: ReleaseItem[] }) 
                   </div>
                 </div>
                 <div className="flex w-full flex-col pr-2">
-                  <h3 className="text-[16px] font-black leading-tight text-slate-800 dark:text-white">
+                  <h3 className="mt-4 text-[16px] font-black leading-tight text-slate-800 dark:text-white">
                     {release.releaseTitle}
                   </h3>
                   <p className="text-sm line-clamp-3 text-slate-500 dark:text-gray-400">
