598d4da5b1c23c4bc33bfb5c5071362547a84027
Arley Cuadrado Sierra <arley.cuadradosierra@gmail.com>
2026-05-27 00:13:06 -0500

    Remove top padding from post detail layout

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -53,7 +53,7 @@ export default async function Post({ params: paramsPromise }: Args) {
   if (!post) return <PayloadRedirects url={url} />
 
   return (
-    <article className="mx-auto max-w-4xl pt-16 pb-16">
+    <article className="mx-auto max-w-4xl pb-16">
       <PageClient />
 
       {/* Allows redirects for valid pages too */}
