d6ae3d4e4fcc9258447359308d6ed33b7c20f510
Arley Cuadrado Sierra <arley.cuadradosierra@gmail.com>
2026-06-10 18:50:43 -0500

    Tighten mobile layout spacing around fixed navigation

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -37,7 +37,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             />
 
             <Header />
-            <main className="w-full pt-[var(--admin-bar-offset,0px)] max-[975px]:pt-[calc(var(--admin-bar-offset,0px)+5rem)] max-[975px]:pb-[var(--mobile-page-bottom-offset,6rem)]">
+            <main className="w-full pt-[var(--admin-bar-offset,0px)] max-[975px]:pt-[calc(var(--admin-bar-offset,0px)+3.75rem)] max-[975px]:pb-[var(--mobile-page-bottom-offset,3.5rem)]">
               {children}
             </main>
             {/*<Footer />*/}
