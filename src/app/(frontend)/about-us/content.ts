92c8ec35e767431e07a7fdc8946a59a1a64beae7
Arley Cuadrado Sierra <arley.cuadradosierra@gmail.com>
2026-06-11 14:45:39 -0500

    Refresh about page copy and emphasis styling

--- src/app/(frontend)/about-us/content.ts
+++ src/app/(frontend)/about-us/content.ts
@@ -1,6 +1,6 @@
-export const aboutUsDateTime = '2026-05-16'
-export const aboutUsUpdatedAt = '16 de mayo de 2026'
-export const aboutUsIntroTitle = 'Bueno, ¿por qué Oddsound existe?'
+export const aboutUsDateTime = '2026-06-11'
+export const aboutUsUpdatedAt = '11 de junio de 2026'
+export const aboutUsIntroTitle = '¿Por qué existe esta vaina?'
 export const aboutUsTitle = 'Por qué Oddsound?'
 export const aboutUsIntro = [
   'Sin tanto cuento y sin tanta parla, hay un momento donde leer las mismas noticias de los artistas pop en magazines elitistas resulta en un estancamiento extraño. Más cuando interesa descubrir nuevas voces de la escena local - nacional e internacional, y en muchas de esas búsquedas he resultado sorprendido pero a veces la experiencia se ve un poco truncada por lo dispersa de la información.',
