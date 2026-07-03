a58936eb747fbd0ebfdd5525a2d2056de2ace29a
Arley Cuadrado Sierra <arley.cuadradosierra@gmail.com>
2026-07-03 13:30:20 -0500

    Rename content section controls in the editor

--- src/i18n/payloadSpanish.ts
+++ src/i18n/payloadSpanish.ts
@@ -4,11 +4,16 @@ export const payloadSpanish = {
   ...es,
   translations: {
     ...es.translations,
+    fields: {
+      ...es.translations.fields,
+      searchForBlock: 'Buscar sección',
+    },
     general: {
       ...es.translations.general,
       create: 'Añadir imagen',
       createNew: 'Añadir imagen',
       creatingNewLabel: '{{label}}',
+      layout: 'sección',
       successfullyCreated: '{{label}} añadida correctamente',
     },
   },
