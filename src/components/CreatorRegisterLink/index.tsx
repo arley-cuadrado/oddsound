ae783de2f34ff1c509f2c0f6c7b9661ef5b91355
Arley Cuadrado Sierra <arley.cuadradosierra@gmail.com>
2026-09-06 09:36:21 -0500

    Center dashboard login links

--- src/components/CreatorRegisterLink/index.tsx
+++ src/components/CreatorRegisterLink/index.tsx
@@ -3,9 +3,21 @@ import React from 'react'
 
 const CreatorRegisterLink: React.FC = () => {
   return (
-    <div className="flex flex-col items-center space-y-3 text-center">
-      <Link href="/creator/register">Regístrate como artista</Link>
-      <Link href="/fan/login">Inicia sesión como fan</Link>
+    <div
+      style={{
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '12px',
+        textAlign: 'center',
+      }}
+    >
+      <Link href="/creator/register" style={{ textDecoration: 'underline' }}>
+        Regístrate como artista
+      </Link>
+      <Link href="/fan/login" style={{ textDecoration: 'underline' }}>
+        Inicia sesión como fan
+      </Link>
     </div>
   )
 }
